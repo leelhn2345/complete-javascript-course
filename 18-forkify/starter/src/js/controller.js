8a711b06eabaa5d173de0f1caf95b02f32ae3c7a
leelhn2345 <leelhn_95@hotmail.com>
2020-12-25 20:44:16 +0800

    parcel version incompatibility.. class p property

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -1,13 +1,37 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
+// https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+
+const controlRecipes = async () => {
+  try {
+    const id = window.location.hash.slice(1);
+    // const id = '5ed6604591c37cdc054bc886';
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1. load recipe
+    await model.loadRecipe(id);
+
+    // 2. rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    console.log(err);
+    recipeView.renderError(err);
+  }
 };
 
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
+const init = () => {
+  recipeView.addHandleRender(controlRecipes);
+};
+init();
+// ['hashchange', 'load'].forEach(ev =>
+//   window.addEventListener(ev, controlRecipes)
+// );
+// window.addEventListener('hashchange', controlRecipes);
+// window.addEventListener('load', controlRecipes);
